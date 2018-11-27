62e53474b1d79b7ade6abf06367bd8d36c36db9f
simaQ <sima.zhang1990@gmail.com>
2018-11-27 13:44:38 +0800

    添加基础股票图 demo.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,15 @@
 Page({
     data: {
-        charts: [            
+        charts: [
+            { name: 'stock', value: '基础股票图' },
             { name: 'line', value: '折线图' },
             { name: 'area', value: '区域图' },
-            { name: 'stack-area', value: '层叠区域图' },            
+            { name: 'stack-area', value: '层叠区域图' },
             { name: 'column', value: '柱状图' },
             { name: 'dodge', value: '分组柱状图' },
             { name: 'stack-column', value: '层叠柱状图' },
             { name: 'bar', value: '条形图' },
-            { name: 'dodge-bar', value: '分组条形图' },    
+            { name: 'dodge-bar', value: '分组条形图' },
             { name: 'stack-bar', value: '层叠条形图' },
             { name: 'ring', value: '环图' },
             { name: 'pie', value: '饼图' },
@@ -18,7 +19,7 @@ Page({
             { name: 'double-axis', value: '双 Y 轴' }
         ],
         interactions: [
-            { name: 'selection-for-bar-chart', value: '柱状图选中交互' },            
+            { name: 'selection-for-bar-chart', value: '柱状图选中交互' },
             { name: 'selection-for-pie-chart', value: '饼图选中交互' },
             { name: 'pan-bar-chart', value: '柱状图平移交互' },
             { name: 'pan-line-chart', value: '折线图平移交互' }
